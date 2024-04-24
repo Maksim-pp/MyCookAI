2aba478f2b2ea424873d8cb87d8acb900e81fc00
Maksim <100757585+Maksim-pp@users.noreply.github.com>
2024-04-24 12:28:51 +0300

    bag

--- src/shared/constants/buttons-library-page.ts
+++ src/shared/constants/buttons-library-page.ts
@@ -1,21 +1,24 @@
 import { CREATE_RECIPE_PATH, RECIPES, SHOP_LIST_PATH } from "./path";
+import Foto1 from 'src/app/assets/images/personal-space-1.png';
+import Foto2 from 'src/app/assets/images/personal-space-2.png';
+import Foto3 from 'src/app/assets/images/personal-space-3.png';
 
 export const button = [
     {
         id:1,
-        img: '/src/app/assets/images/personal-space-1.png',
+        img: Foto1,
         text: 'Сгенерировать рецепт',
         path: CREATE_RECIPE_PATH,
     },
     {
         id:2,
-        img: '/src/app/assets/images/personal-space-2.png',
+        img: Foto2,
         text: 'Библиотека рецептов',
         path: RECIPES,
     },
     {
         id:3,
-        img: '/src/app/assets/images/personal-space-3.png',
+        img: Foto3,
         text: 'Планирование рецепта',
         path: SHOP_LIST_PATH, 
     }
